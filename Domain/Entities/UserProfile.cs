b7841676381f997198d648a0a8fce433ecd99bca
Iqbal Danish <iq.danish@gmail.com>
2024-09-15 00:05:27 +0530

    Adding the bare minimum folder and files in the Domain Project
    Adding the bare minimum folder and files in the Domain Project

---
+++ Domain/Entities/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Primitives;
+
+namespace Domain.Entities
+{ 
+    public sealed class UserProfile : Entity
+    {
+        public UserProfile(Guid id, string firstName, string lastName): base(id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }        
+
+    }
+}
