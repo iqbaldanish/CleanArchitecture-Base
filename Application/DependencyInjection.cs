e58534b2497a9c9128bf3b3998df13baa5aeb7a3
Iqbal Danish <iq.danish@gmail.com>
2024-09-15 18:18:41 +0530

    Adding Dependency Injection class at each layer. Also adding CQRS and Fluent API support at Application Layer
    Adding Dependency Injection class at each layer. Also adding CQRS and Fluent API support at Application Layer

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Extension method to register application-specific services.
+        /// Adds MediatR services from the current assembly for handling CQRS patterns (commands, queries, events) 
+        /// and FluentValidation for registering all validators from the current assembly.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to which services are added.</param>
+        /// <returns>The modified IServiceCollection with registered services.</returns>
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {            
+            var assembly = typeof(DependencyInjection).Assembly;            
+            services.AddMediatR(configuration =>
+                configuration.RegisterServicesFromAssembly(assembly));
+            
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
--- Web.API/Program.cs
+++ Web.API/Program.cs
@@ -1,3 +1,8 @@
+using Application;
+using Infrastructure;
+using Persistence;
+using Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Registering services from all the inner layers of CLEAN ARCHITECTURE
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPersistence()
+    .AddPresentation();
 
 var app = builder.Build();
 
