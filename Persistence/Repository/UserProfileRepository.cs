e887fdb585df034ef048e70c083683482eb5ea61
Iqbal Danish <iq.danish@gmail.com>
2024-09-17 13:57:12 +0530

    Added Repository in Persistence Layer for UserProfile
    Added Repository in Persistence Layer for UserProfile

---
+++ Persistence/Repository/UserProfileRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+
+namespace Persistence.Repository
+{
+    internal sealed class UserProfileRepository : IUserProfileRepository
+    {
+        public async Task<UserProfile?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            await Task.CompletedTask;
+            return null;
+        }
+
+        public void Insert(UserProfile userProfile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
